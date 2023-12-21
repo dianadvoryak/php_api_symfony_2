05af14b29a47f66f4651081a91126766e82dc6c8
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-12-21 06:01:06 +0200

    nelmio/api-doc-bundle dont work

--- src/Controller/BookCategoryController.php
+++ src/Controller/BookCategoryController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Model\BookCategoryListResponse;
 use App\Service\BookCategoryService;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class BookCategoryController extends AbstractController
 {
@@ -13,7 +16,12 @@ class BookCategoryController extends AbstractController
     {
     }
 
-    #[Route('/api/v1/book/categories')]
+    #[OA\Response(
+        response: 200,
+        description: 'Return book categories',
+        content: new Model(type: BookCategoryListResponse::class)
+    )]
+    #[Route(path: '/api/v1/book/categories', methods: ['GET'])]
     public function categories(): Response
     {
         return $this->json($this->bookCategoryService->getCategories());
